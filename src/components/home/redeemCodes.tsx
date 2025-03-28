7280076ccb8a4d765297052a04d488fc4fbfcf62
Nilesh Viduranga <nnviduranga@gmail.com>
2025-03-28 20:20:30 +0530

    Add react-toastify for notifications and enhance RedeemCodes component with clipboard functionality

--- src/components/home/redeemCodes.tsx
+++ src/components/home/redeemCodes.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { ClipboardDocumentListIcon } from "@heroicons/react/16/solid";
+import { ToastContainer, toast } from "react-toastify";
+
 import { getRedeemCodes } from "~/services/system/system.service";
+import yaeWispherIcon from "~/assets/icons/system/yae_wispher.png";
 
 export default function RedeemCodes() {
   const [redeemCodes, setRedeemCodes] = useState<IRedeemCodeResponse>({
@@ -14,6 +18,19 @@ export default function RedeemCodes() {
     setRedeemCodes(data);
   };
 
+  const notify = (message: string) => {
+    toast.success(message, {
+      position: "bottom-center",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      theme: "dark",
+      icon: <img src={yaeWispherIcon.src} alt="Yae Miko" className="w-8" />,
+    });
+  };
+
   useEffect(() => {
     fetchRedeemCodes();
   }, []);
@@ -23,22 +40,30 @@ export default function RedeemCodes() {
       <h2 className="text-2xl text-white text-center xl:text-left w-full">
         Redeem Codes
       </h2>
-      <div className="w-full items-center justify-center grid grid-cols-1 lg:grid-cols-2 lg:gap-2">
+      <div className="w-full items-center justify-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-2 md:gap-2">
         {redeemCodes["active"].map((code) => (
           <div
             key={code.code}
-            className="w-full px-4 py-2 bg-slate-800 rounded-lg"
+            className="w-full px-4 py-2 bg-slate-800 rounded-lg flex items-center justify-between space-x-2"
             style={{
               boxShadow: "0 4px 8px rgba(0, 0, 0, 0.6)",
               backgroundColor: "rgba(16, 24, 40, 0.5)",
             }}
           >
-            <p className="text-white font-semibold font-enka text-center w-full">
+            <p className="text-white font-semibold font-enka text-left w-full xl:text-sm 2xl:text-md">
               {code.code}
             </p>
+            <ClipboardDocumentListIcon
+              className="w-6 h-6 text-white mx-auto cursor-pointer"
+              onClick={() => {
+                navigator.clipboard.writeText(code.code);
+                notify("Code copied to clipboard!");
+              }}
+            />
           </div>
         ))}
       </div>
+      <ToastContainer />
     </div>
   );
 }
