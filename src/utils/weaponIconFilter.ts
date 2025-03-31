9e5c4030c89aa5198857dc97a32070794a0c00cf
Nilesh Viduranga <nnviduranga@gmail.com>
2025-03-31 18:59:01 +0530

    Refactor weapon secondary type handling: remove old definitions, introduce new TypeScript types, and update icon mapping for improved consistency and maintainability

--- src/utils/weaponIconFilter.ts
+++ src/utils/weaponIconFilter.ts
@@ -28,6 +28,7 @@ import GeoDmgBonusIcon from "~/assets/icons/stats/FIGHT_PROP_ROCK_ADD_HURT.svg";
 import ShieldCostReductionIcon from "~/assets/icons/stats/FIGHT_PROP_SHIELD_COST_MINUS_RATIO.svg";
 import HydroDmgBonusIcon from "~/assets/icons/stats/FIGHT_PROP_WATER_ADD_HURT.svg";
 import AnemoDmgBonusIcon from "~/assets/icons/stats/FIGHT_PROP_WIND_ADD_HURT.svg";
+import { IWeaponSecondaryTypeKey } from "~/types/enka/weaponSecondaryType";
 
 const weaponTypeIconFilter = {
   WEAPON_SWORD_ONE_HAND: SwordIcon,
@@ -38,29 +39,83 @@ const weaponTypeIconFilter = {
 };
 
 const weaponStatIconMap = new Map<string, StaticImageData>();
-weaponStatIconMap.set("FIGHT_PROP_ATTACK_PERCENT", AtkPercentIcon);
-weaponStatIconMap.set("FIGHT_PROP_ATTACK", AtkIcon);
-weaponStatIconMap.set("FIGHT_PROP_BASE_ATTACK", BaseAtkIcon);
-weaponStatIconMap.set("FIGHT_PROP_CHARGE_EFFICIENCY", EnergyRechargeIcon);
-weaponStatIconMap.set("FIGHT_PROP_CRITICAL", CritRateIcon);
-weaponStatIconMap.set("FIGHT_PROP_CRITICAL_HURT", CritDmgIcon);
-weaponStatIconMap.set("FIGHT_PROP_DEFENSE_PERCENT", DefPercentIcon);
-weaponStatIconMap.set("FIGHT_PROP_DEFENSE", DefIcon);
-weaponStatIconMap.set("FIGHT_PROP_ELEMENT_MASTERY", EMIcon);
-weaponStatIconMap.set("FIGHT_PROP_ELEC_ADD_HURT", ElecDmgBonusIcon);
-weaponStatIconMap.set("FIGHT_PROP_FIRE_ADD_HURT", PyroDmgBonusIcon);
-weaponStatIconMap.set("FIGHT_PROP_GRASS_ADD_HURT", DendroDmgBonusIcon);
-weaponStatIconMap.set("FIGHT_PROP_ICE_ADD_HURT", CryoDmgBonusIcon);
-weaponStatIconMap.set("FIGHT_PROP_ROCK_ADD_HURT", GeoDmgBonusIcon);
-weaponStatIconMap.set("FIGHT_PROP_WATER_ADD_HURT", HydroDmgBonusIcon);
-weaponStatIconMap.set("FIGHT_PROP_WIND_ADD_HURT", AnemoDmgBonusIcon);
-weaponStatIconMap.set("FIGHT_PROP_PHYSICAL_ADD_HURT", PhysicalDmgBonusIcon);
-weaponStatIconMap.set("FIGHT_PROP_HEAL_ADD", HealingBonusIcon);
-weaponStatIconMap.set("FIGHT_PROP_HEALED_ADD", HealingReceivedBonusIcon);
-weaponStatIconMap.set("FIGHT_PROP_HP_PERCENT", HPPercentIcon);
-weaponStatIconMap.set("FIGHT_PROP_HP", HPIcon);
 weaponStatIconMap.set(
-  "FIGHT_PROP_SHIELD_COST_MINUS_RATIO",
+  IWeaponSecondaryTypeKey.FIGHT_PROP_ATTACK_PERCENT,
+  AtkPercentIcon
+);
+weaponStatIconMap.set(IWeaponSecondaryTypeKey.FIGHT_PROP_ATTACK, AtkIcon);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_BASE_ATTACK,
+  BaseAtkIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_CHARGE_EFFICIENCY,
+  EnergyRechargeIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_CRITICAL,
+  CritRateIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_DEFENSE_PERCENT,
+  DefPercentIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_CRITICAL_HURT,
+  CritDmgIcon
+);
+weaponStatIconMap.set(IWeaponSecondaryTypeKey.FIGHT_PROP_DEFENSE, DefIcon);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_ELEMENT_MASTERY,
+  EMIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_ELEC_ADD_HURT,
+  ElecDmgBonusIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_FIRE_ADD_HURT,
+  PyroDmgBonusIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_GRASS_ADD_HURT,
+  DendroDmgBonusIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_ICE_ADD_HURT,
+  CryoDmgBonusIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_ROCK_ADD_HURT,
+  GeoDmgBonusIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_WATER_ADD_HURT,
+  HydroDmgBonusIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_WIND_ADD_HURT,
+  AnemoDmgBonusIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_PHYSICAL_ADD_HURT,
+  PhysicalDmgBonusIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_HEAL_ADD,
+  HealingBonusIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_HEALED_ADD,
+  HealingReceivedBonusIcon
+);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_HP_PERCENT,
+  HPPercentIcon
+);
+weaponStatIconMap.set(IWeaponSecondaryTypeKey.FIGHT_PROP_HP, HPIcon);
+weaponStatIconMap.set(
+  IWeaponSecondaryTypeKey.FIGHT_PROP_SHIELD_COST_MINUS_RATIO,
   ShieldCostReductionIcon
 );
 
