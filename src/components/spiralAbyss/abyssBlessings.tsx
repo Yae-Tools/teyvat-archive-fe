8647dde3196291afcbdeed8a28cb74dbb5e0dadd
Nilesh Viduranga <nnviduranga@gmail.com>
2025-04-02 00:16:14 +0530

    Refactor AbyssBlessings component: extract BlessingItem component for improved readability and maintainability.

--- src/components/spiralAbyss/abyssBlessings.tsx
+++ src/components/spiralAbyss/abyssBlessings.tsx
@@ -1,11 +1,10 @@
-import { format } from "date-fns";
-import { CalendarIcon, SwordsIcon } from "lucide-react";
+
 
 import { IAbyssBlessing } from "~/types/enka/enka.types";
-import { isCurrentBlessing } from "~/utils/parsers/abyssDataParser";
-import parseText from "~/utils/parsers/parseEnkaText";
 
 import TitleHeading from "../common/typography/titleHeading";
+
+import BlessingItem from "./blessingItem";
 
 type Props = {
   sortedAbyssBlessings: IAbyssBlessing[];
@@ -22,46 +21,7 @@ export default function AbyssBlessings({
       />
       <div className="mt-4 grid w-full grid-cols-1 justify-items-center gap-4 md:grid-cols-3 lg:grid-cols-4">
         {sortedAbyssBlessings.map((blessing) => (
-          <article
-            className="max-w-[300px] rounded-lg bg-slate-700"
-            key={blessing.id}
-          >
-            <div className="flex items-start p-4">
-              <div>
-                <h3 className="mb-2 font-medium sm:text-lg">{blessing.name}</h3>
-                <div
-                  className="text-sm text-white"
-                  dangerouslySetInnerHTML={{
-                    __html: parseText(blessing.description)
-                  }}
-                />
-                <div className="mt-2 sm:flex sm:items-center sm:gap-2">
-                  <div className="flex items-center gap-1 text-gray-500">
-                    <CalendarIcon className="size-4 text-slate-200" />
-                    <p className="text-xs text-slate-200">
-                      {format(blessing.begin, "do 'of' MMM yyyy")}
-                    </p>
-                  </div>
-                  <span className="hidden sm:block" aria-hidden="true">
-                    ·
-                  </span>
-                  <div className="flex items-center gap-1 text-gray-500">
-                    <p className="text-xs text-slate-200">
-                      {format(blessing.end, "do 'of' MMM yyyy")}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {isCurrentBlessing(blessing.begin, blessing.end) && (
-              <div className="flex justify-end">
-                <strong className="-me-[2px] -mb-[2px] inline-flex items-center gap-1 rounded-ss-xl rounded-ee-xl bg-teal-600 px-3 py-1.5 text-white">
-                  <SwordsIcon className="size-4" />
-                  <span className="text-sm font-medium">Current Blessing</span>
-                </strong>
-              </div>
-            )}
-          </article>
+          <BlessingItem {...{ blessing }} key={blessing.id} />
         ))}
       </div>
     </div>
