74a7c6359d7cf2f6be1386122ad2182728ac5021
Nilesh Viduranga <nnviduranga@gmail.com>
2025-04-09 00:26:46 +0530

    feat: update dependencies and refactor artifact overview components for improved structure and functionality

--- src/components/artifacts/artifactOverview/artifactOverviewDesktop.tsx
+++ src/components/artifacts/artifactOverview/artifactOverviewDesktop.tsx
@@ -1,61 +1,26 @@
-import { useAtom } from "jotai";
-
-import {
-  selectedArtifactEquipTypeAtom,
-  selectedRarityFullSetAtom
-} from "~/atoms/teyvat/artifact.atom";
-import DescriptionDesktop from "~/components/common/descriptionDesktop";
 import OverviewItemHolder from "~/components/common/overviewItemHolder";
 import { ISetBonus } from "~/types/enka/artifacts.types";
 
-import ArtifactEquipPicker from "../artifactEquipPicker/artifactEquipPicker";
-
 type Props = {
-  description: string;
   setBonus: ISetBonus[];
 };
 
-export default function ArtifactOverviewDesktop({
-  description,
-  setBonus
-}: Readonly<Props>) {
-  const [selectedArtifactEquipType, setSelectedArtifactEquipType] = useAtom(
-    selectedArtifactEquipTypeAtom
-  );
-  const [selectedRarityFullSet] = useAtom(selectedRarityFullSetAtom);
-
+export default function ArtifactOverviewDesktop({ setBonus }: Readonly<Props>) {
   return (
-    <div className="absolute right-0 bottom-0 mx-3 flex h-max w-3/5 flex-col items-end justify-end rounded-xl bg-black/40">
-      <div className="my-4 flex w-full items-start justify-end px-4">
-        <DescriptionDesktop description={description} align="right" />
-      </div>
-      <div className="my-2 flex w-full space-x-1 px-4">
-        <div className="flex h-full max-h-[220px] w-full items-start justify-end overflow-y-auto">
+    <div className="mx-3 flex h-full w-3/5 flex-col items-start justify-start rounded-xl bg-black/40">
+      <div className="my-2 flex h-full w-full items-start justify-end space-x-1 overflow-y-auto px-4">
+        <OverviewItemHolder
+          label={`${setBonus[0].needCount}-Piece Bonus`}
+          value={setBonus[0].description}
+          align="start"
+          valueCustomClass="text-slate-300 xl:text-white leading-4 xl:leading-5 text-left mt-2"
+        />
+        {setBonus.length > 1 && (
           <OverviewItemHolder
-            label={`${setBonus[0].needCount}-Piece Bonus`}
-            value={setBonus[0].description}
-            align="end"
-            valueCustomClass="text-slate-300 xl:text-white xl:text-xl font-semibold leading-4 xl:leading-5 text-right"
-          />
-          {setBonus.length > 1 && (
-            <OverviewItemHolder
-              label={`${setBonus[1].needCount}-Piece Bonus`}
-              value={setBonus[1].description}
-              align="end"
-              valueCustomClass="text-slate-300 xl:text-white xl:text-lg font-semibold leading-4 xl:leading-5 text-right"
-            />
-          )}
-        </div>
-      </div>
-
-      <div className="my-2 flex w-full items-center justify-end px-6">
-        {selectedRarityFullSet.length > 0 && (
-          <ArtifactEquipPicker
-            {...{
-              selectedArtifactEquipType,
-              setSelectedArtifactEquipType,
-              selectedRarityFullSet
-            }}
+            label={`${setBonus[1].needCount}-Piece Bonus`}
+            value={setBonus[1].description}
+            align="start"
+            valueCustomClass="text-slate-300 xl:text-white leading-4 xl:leading-5 text-left mt-2"
           />
         )}
       </div>
--- src/components/artifacts/artifactProfile/artifactProfileDesktop.tsx
+++ src/components/artifacts/artifactProfile/artifactProfileDesktop.tsx
@@ -1,10 +1,16 @@
+import { useAtom } from "jotai";
 import Image from "next/image";
 
+import {
+  selectedArtifactEquipTypeAtom,
+  selectedRarityFullSetAtom
+} from "~/atoms/teyvat/artifact.atom";
 import RarityStars from "~/components/common/rarityStars";
 import TitleHeading from "~/components/common/typography/titleHeading";
 import { EQUIP_TYPES } from "~/data/teyvatData";
 import { IEquipCollection, ISetBonus } from "~/types/enka/artifacts.types";
 
+import ArtifactEquipPicker from "../artifactEquipPicker/artifactEquipPicker";
 import ArtifactOverviewDesktop from "../artifactOverview/artifactOverviewDesktop";
 
 type Props = {
@@ -21,45 +27,49 @@ export default function ArtifactProfileDesktop({
   setBonus
 }: Readonly<Props>) {
   const { name, icon, description } = selectedEquipItem;
+
+  const [selectedArtifactEquipType, setSelectedArtifactEquipType] = useAtom(
+    selectedArtifactEquipTypeAtom
+  );
+  const [selectedRarityFullSet] = useAtom(selectedRarityFullSetAtom);
   return (
     <>
-      <div className="m-2 flex w-full items-start justify-between">
-        <div className="mx-2 flex w-1/3 flex-col items-start">
+      <div className="m-2 flex w-full flex-col items-start justify-start">
+        <div className="mx-2 flex w-max flex-col items-start">
           <div className="mb-2 flex items-center justify-start space-x-1">
             <RarityStars stars={highestRarity} />
           </div>
         </div>
-        <div className="mx-2 flex w-2/3 flex-col items-end justify-end">
-          <div className="mb-2 flex w-full items-center justify-end space-x-1 text-right">
+        <div className="flex w-full items-start justify-between space-x-1">
+          <div className="mx-2 mb-2 flex w-2/3 items-start justify-start space-x-1 text-left">
             <TitleHeading text={name} />
           </div>
-          <div className="mb-2 flex w-full items-center justify-end space-x-1 text-right">
-            <h6 className="text-right text-xl text-white">
-              {setName} | {EQUIP_TYPES[selectedEquipItem.equipType]}
-            </h6>
+          <div className="my-2 flex w-1/3 flex-col items-center justify-end px-6">
+            {selectedRarityFullSet.length > 0 && (
+              <ArtifactEquipPicker
+                {...{
+                  selectedArtifactEquipType,
+                  setSelectedArtifactEquipType,
+                  selectedRarityFullSet
+                }}
+              />
+            )}
+            <div className="my-2 flex w-full flex-col items-end justify-end space-x-1 text-right text-white">
+              <h6 className="text-xl">{setName}</h6>
+              <h6>{EQUIP_TYPES[selectedEquipItem.equipType]}</h6>
+            </div>
           </div>
         </div>
       </div>
       <div className="relative mx-2 flex h-full w-full">
-        <div className="mx-2 flex w-1/3 flex-col items-start">
-          <Image
-            src={icon}
-            alt={name}
-            height={200}
-            width={500}
-            className="absolute bottom-10 left-10"
-            style={{
-              height: "100%",
-              scale: "1"
-            }}
-          />
-        </div>
         <ArtifactOverviewDesktop
           {...{
-            description,
             setBonus
           }}
         />
+        <div className="mx-2 flex w-1/3 flex-col items-center justify-center p-4">
+          <Image src={icon} alt={name} height={300} width={300} />
+        </div>
       </div>
     </>
   );
