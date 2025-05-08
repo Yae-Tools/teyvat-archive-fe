2a5b5e0400576091cbb107e3ef22e706f597d378
Nilesh.V <nilesh.v@intervest.lk>
2025-05-08 15:53:44 +0530

    feat: enhance MiniAvatar and RewardUsers components by adding skillDepotId and nameId properties for better character data handling and adding navigation to details page

--- src/types/enka/domain.types.ts
+++ src/types/enka/domain.types.ts
@@ -14,6 +14,7 @@ export interface IRewardUsedByCharacter {
   id: number;
   name: string;
   iconUrl: string;
+  skillDepotId: number;
   isTraveler: boolean;
   rarity: IRarityType;
   element: IElementType;
