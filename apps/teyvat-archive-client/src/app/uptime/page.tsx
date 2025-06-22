e5d52972f81166c9c06ad8b3fd0ecdfa2404040c
azula9713 <nnviduranga@gmail.com>
2025-06-22 14:20:11 +0530

    Add uptime status display for Teyvat Archive components

--- apps/teyvat-archive-client/src/app/uptime/page.tsx
+++ apps/teyvat-archive-client/src/app/uptime/page.tsx
@@ -4,6 +4,21 @@ export default function UptimePage() {
   return (
     <div className="mt-3 flex w-full flex-col items-center justify-center xl:mb-4">
       <PageTitle title="Uptime Status" />
+      {/* <div>Overall Uptime</div> */}
+      <div className="mt-4 flex w-full flex-col items-center justify-center gap-4 px-4 xl:w-1/2">
+        <div className="w-full rounded-lg border-2 border-amber-300">
+          Teyvat Archive
+        </div>
+        <div className="w-full rounded-lg border-2 border-amber-300">
+          Teyvat Archive Server
+        </div>
+        <div className="w-full rounded-lg border-2 border-amber-300">
+          Teyvat Archive Dashboard
+        </div>
+        <div className="w-full rounded-lg border-2 border-amber-300">
+          Teyvat Archive CDN
+        </div>
+      </div>
     </div>
   );
 }
